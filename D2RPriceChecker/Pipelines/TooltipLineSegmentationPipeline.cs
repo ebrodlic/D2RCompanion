9082ab07f6cbb080594ea9b5f634f2a01e9f106e
Edin Brodlic <ebrodlic@outlook.com>
2026-03-21 16:05:37 +0100

    minor tweak

--- D2RPriceChecker/Pipelines/TooltipLineSegmentationPipeline.cs
+++ D2RPriceChecker/Pipelines/TooltipLineSegmentationPipeline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO.Pipelines;
 using System.Text;
@@ -13,13 +14,7 @@ namespace D2RPriceChecker.Pipelines
         public int _maxChannelThreshold { get; set; } = 140;
         public double _densityThresholdRatio { get; set; } = 0.005;
         public int _padding { get; set; } = 6;
-
-        // Row Density Calculation/Brightness thresholding
-        // - Iterate through each row of the tooltip image and calculate the average brightness.    
-
-        // go through each row of the tooltip image and calculate the average brightness
-        // if the average brightness is above a certain threshold, consider it as a line break
-        // store the positive lines in a list, until we hit empty line
+ 
         public TooltipLineSegmetnationPipelineResult Run(Bitmap tooltip)
         {
             var result = new TooltipLineSegmetnationPipelineResult(tooltip);
@@ -30,7 +25,91 @@ namespace D2RPriceChecker.Pipelines
             result.TooltipLines = lines;
 
             return result;
+        }       
+
+        private bool[] GetRowsContentMask(Bitmap tooltip, bool smootheResults = true) 
+        {
+            var minDensity = tooltip.Width * _densityThresholdRatio;
+
+            bool[] rowsMap = new bool[tooltip.Height];
+
+            for (int y = 0; y < tooltip.Height; y++)
+            {
+                var count = 0;
+
+                for (int x = 0; x < tooltip.Width; x++)
+                {
+                    Color pixel = tooltip.GetPixel(x, y);
+
+
+                    // TODO: remove after debugging
+                    if (x == 256 && y == 41)
+                    {
+                        Debug.WriteLine($"Pixel at (256, 41): R={pixel.R}, G={pixel.G}, B={pixel.B}");
+                    }
+
+                    if (IsPixelContent(pixel))
+                        count++;
+                }
+
+                rowsMap[y] = count > minDensity ? true : false;
+            }
+
+            if(smootheResults)  
+                FillShortGaps(rowsMap, _padding);
+           
+            return rowsMap;
         }
+
+        private void FillShortGaps(bool[] rowsMask, int minGapSize)
+        {
+            int startIdx = -1;  // start of a zero sequence
+
+            for (int i = 0; i < rowsMask.Length; i++)
+            {
+                if (!rowsMask[i])
+                {
+                    if (startIdx == -1)
+                        startIdx = i;  // mark the start of a zero sequence
+                }
+                else
+                {
+                    if (startIdx != -1)
+                    {
+                        int gapSize = i - startIdx;
+                        if (gapSize < minGapSize)
+                        {
+                            // Fill the gap with true values
+                            for (int j = startIdx; j < i; j++)
+                                rowsMask[j] = true;
+                        }
+                        startIdx = -1;  // reset for the next sequence
+                    }
+                }
+            }
+            //handle trailing zeroes at the end?
+        }
+
+        private bool IsPixelContent(Color pixel)
+        {
+            int r = pixel.R;
+            int g = pixel.G;
+            int b = pixel.B;
+
+            float brightness = (0.299f * pixel.R) + (0.587f * pixel.G) + (0.114f * pixel.B);
+            int maxChannel = Math.Max(pixel.R, Math.Max(pixel.G, pixel.B));
+
+            var isContent = brightness > _brightnessThreshold || maxChannel > _maxChannelThreshold;
+
+            return isContent;
+        }
+
+        //private bool IsPixelTextColor(Color pixel)
+        //{
+        //    bool isYellow = hue >= 40 && hue <= 65 && saturation > 0.4 && value > 0.5;
+        //    bool isBlue = hue >= 180 && hue <= 250 && saturation > 0.4;
+        //    bool isGreen = hue >= 80 && hue <= 160 && saturation > 0.4;
+        //}
 
         private List<Bitmap> GetLinesBitmaps(Bitmap tooltip, bool[] rowsMask, int padding)
         {
@@ -66,7 +145,7 @@ namespace D2RPriceChecker.Pipelines
                             {
                                 xIndexStart = Math.Min(xIndexStart, x);
                                 xIndexEnd = Math.Max(xIndexEnd, x);
-                            } 
+                            }
                         }
                     }
                 }
@@ -102,74 +181,6 @@ namespace D2RPriceChecker.Pipelines
             }
 
             return lines;
-        }
-
-        private bool[] GetRowsContentMask(Bitmap tooltip, bool smootheResults = true) 
-        {
-            var minDensity = tooltip.Width * _densityThresholdRatio;
-
-            bool[] rowsMap = new bool[tooltip.Height];
-
-            for (int y = 0; y < tooltip.Height; y++)
-            {
-                var count = 0;
-
-                for (int x = 0; x < tooltip.Width; x++)
-                {
-                    Color pixel = tooltip.GetPixel(x, y);
-
-                    if (IsPixelContent(pixel))
-                        count++;
-                }
-
-                rowsMap[y] = count > minDensity ? true : false;
-            }
-
-            if(smootheResults)  
-                FillShortGaps(rowsMap, _padding);
-           
-            return rowsMap;
-        }
-
-        private bool IsPixelContent(Color pixel)
-        {
-            int r = pixel.R;
-            int g = pixel.G;
-            int b = pixel.B;
-
-            float brightness = (0.299f * pixel.R) + (0.587f * pixel.G) + (0.114f * pixel.B);
-            int maxChannel = Math.Max(pixel.R, Math.Max(pixel.G, pixel.B));
-
-            return brightness > _brightnessThreshold || maxChannel > _maxChannelThreshold;
-        }
-
-        private void FillShortGaps(bool[] rowsMask, int minGapSize)
-        {
-            int startIdx = -1;  // start of a zero sequence
-
-            for (int i = 0; i < rowsMask.Length; i++)
-            {
-                if (!rowsMask[i])
-                {
-                    if (startIdx == -1)
-                        startIdx = i;  // mark the start of a zero sequence
-                }
-                else
-                {
-                    if (startIdx != -1)
-                    {
-                        int gapSize = i - startIdx;
-                        if (gapSize < minGapSize)
-                        {
-                            // Fill the gap with true values
-                            for (int j = startIdx; j < i; j++)
-                                rowsMask[j] = true;
-                        }
-                        startIdx = -1;  // reset for the next sequence
-                    }
-                }
-            }
-            //handle trailing zeroes at the end?
         }
     }
 }
